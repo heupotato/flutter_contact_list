213cf379dfeb7c793e9e75d2d09f6197d41c6ff1
LoanThai <thloanth.dut@gmail.com>
2021-06-18 10:41:02 +0700

    Create UI for list of contacts

---
+++ lib/ui/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_contact_list/data/contact-list-database.dart';
+import 'package:provider/provider.dart'; 
+void main() => runApp(MyApp()); 
+
+class MyApp extends StatelessWidget{
+    @override
+    Widget build(BuildContext context){
+       return Provider(
+         create: (_) => ContactListDatabase(), 
+         child: MaterialApp(
+           title: 'Contact List', 
+           home: ContactList(),
+          ),
+        ); 
+    }
+}
+
