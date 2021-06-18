ed595c8c5f79ced15125988d697dd757f69c721e
LoanThai <thloanth.dut@gmail.com>
2021-06-18 10:45:26 +0700

    Update files locatiom
    Update import location of widget_test since main.dart's location was changed

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_contact_list/data/contact-list-database.dart';
+import 'package:flutter_contact_list/ui/contact-list.dart';
 import 'package:provider/provider.dart'; 
 void main() => runApp(MyApp()); 
 
