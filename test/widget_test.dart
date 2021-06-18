f9c1aedb7c0b5e55bdcace5a47c17e76eaad6222
LoanThai <thloanth.dut@gmail.com>
2021-06-18 11:15:25 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_contact_list/ui/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_contact_list/ui/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
