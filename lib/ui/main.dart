59ad3b34d4822be0ba1b742806339273f18d5bd2
LoanThai <thloanth.dut@gmail.com>
2021-06-18 11:15:18 +0700

    Remove provider package

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_contact_list/data/contact-list-database.dart';
 import 'package:flutter_contact_list/ui/contact-list.dart';
-import 'package:provider/provider.dart'; 
 void main() => runApp(MyApp()); 
 
 class MyApp extends StatelessWidget{
     @override
     Widget build(BuildContext context){
-       return Provider(
-         create: (_) => ContactListDatabase(), 
-         child: MaterialApp(
-           title: 'Contact List', 
-           home: ContactList(),
-          ),
-        ); 
+      return MaterialApp(
+        title: "My contact list",
+        home: ContactList(),
+      ); 
     }
 }
 
