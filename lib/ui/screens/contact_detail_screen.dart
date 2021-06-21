1bf4f192497f63a7724b4cec2a925e3458cf8738
LoanThai <thloanth.dut@gmail.com>
2021-06-21 10:52:42 +0700

    Add optional fields to mock data

--- lib/ui/screens/contact_detail_screen.dart
+++ lib/ui/screens/contact_detail_screen.dart
@@ -3,13 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_contact_list/mockdata/mock_contact.dart';
 
 class ContactDetail extends StatelessWidget {
-    Contact contact; 
+    final Contact contact; 
     ContactDetail({Key? key, required this.contact}) : super(key: key); 
     @override
     Widget build(BuildContext context) {
         //get full information of contact by id 
-        contact = MockContact.mockContact(contact.id); 
-        var contactName = contact.firstName + " " + contact.lastName + contact.id.toString();
+        Contact contactInfo = MockContact.mockContact(contact.id); 
+        var contactName = contactInfo.firstName + " " + contactInfo.lastName + contactInfo.id.toString();
         return Scaffold(
         appBar: AppBar(
             title: Text(contactName),
