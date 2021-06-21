1243276ea0dcf00dab2bd5400f7bf9ab4f9fb865
LoanThai <thloanth.dut@gmail.com>
2021-06-21 08:39:42 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_contact_list/ui/main.dart';
+import 'package:flutter_contact_list/ui/screens/full_screen.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
