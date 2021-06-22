99d63f706c148c80149cf946fedd5bbd125c21cd
LoanThai <thloanth.dut@gmail.com>
2021-06-22 10:13:07 +0700

    Update screen contact's detailed information

--- lib/ui/screens/contact_detail_screen.dart
+++ lib/ui/screens/contact_detail_screen.dart
@@ -5,8 +5,17 @@ import 'package:flutter_contact_list/pic/assets.dart';
 
 class ContactDetail extends StatelessWidget {
     final Contact contact; 
-    final fields = ["First name", "Last name", "Phone number:", "Gender", "Address", "Email"]; 
-    ContactDetail({Key? key, required this.contact}) : super(key: key);
+
+    final fields = ["First name", "Last name", "Address", "Gender", "Email"]; 
+    final divider = Divider(
+        color: Colors.teal[900], 
+        thickness: 3,
+        indent: 200,
+        endIndent: 200,
+    ); 
+
+
+    ContactDetail({Key? key, required this.contact}) : super(key: key); 
 
     List<Widget> appBarPic(String contactName){
         return [Image.asset(
@@ -15,15 +24,79 @@ class ContactDetail extends StatelessWidget {
                         height: 200,
                     ),
                 SizedBox(height: 20), 
-                Text(contactName)
+                Text(contactName, 
+                    style: TextStyle(fontFamily: 'LobsterTwo', fontStyle: FontStyle.italic, 
+                                     fontWeight: FontWeight.w500, fontSize: 35))
         ]; 
     } 
 
     List<String> contactInfoList(Contact contactInfo){
         String gender = contactInfo.gender == 1? "Male" : 
-                        (contactInfo.gender == 2 ? "Female" : "Other"); 
-        return <String> [contactInfo.firstName, contactInfo.lastName, contactInfo.phoneNumber, 
+                        (contactInfo.gender == 2 ? "Female" : ""); 
+        return <String> [contactInfo.firstName, contactInfo.lastName,
                         contactInfo.address, gender, contactInfo.email];  
+        
+    }
+
+    Text titleInfo(String title){
+        return Text(
+            title, 
+            style: TextStyle(fontFamily: 'LobsterTwo', fontSize: 22)
+        );  
+    }
+
+    List<Widget> titleObject(String title){
+        return [
+            SizedBox(height: 20), 
+            titleInfo(title), 
+            divider, 
+        ]; 
+    }
+
+    Row detailInfo(String title, String info){
+        return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+                Spacer(),
+                Expanded(
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                            Text(title + ": ", 
+                            style: TextStyle(fontFamily: 'Peddana', fontSize: 25, fontWeight: FontWeight.w700))
+                        ]
+                    )
+                ),
+                Expanded(
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                            Text(info, 
+                            style: TextStyle(fontFamily: 'Peddana', fontSize: 25))
+                    ]
+                    )
+                ),
+                Spacer()
+            ]
+        ); 
+    }
+
+    List<Row> detailList(List<String> infoList){
+        List<Row> detailList = []; 
+        for (int i = 0; i<infoList.length; i++)
+            detailList.add(detailInfo(fields[i], infoList[i])); 
+        return detailList; 
+    }
+
+    Widget bodyContactDetail(List<String> infoList, String phone, BuildContext context){
+        List<Widget> colChildren = []; 
+        colChildren.addAll(titleObject("Phone")); 
+        colChildren.add(detailInfo("Phone Number", phone)); 
+        colChildren.addAll(titleObject("Detail")); 
+        colChildren.addAll(detailList(infoList)); 
+        return Column(
+            children: colChildren,
+        ); 
     }
 
     @override
@@ -32,26 +105,16 @@ class ContactDetail extends StatelessWidget {
         Contact contactInfo = MockContact.mockContact(contact.id); 
         String contactName = contactInfo.firstName + " " + contactInfo.lastName + contactInfo.id.toString();
         List<String> infoList = contactInfoList(contactInfo); 
+        String phone = contactInfo.phoneNumber; 
         return Scaffold(
             appBar: AppBar(
                 title: Center(child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: appBarPic(contactName),
                     )),
-                actions: [
-                ],
                 toolbarHeight: 300,
             ),
-            body: Container(  
-                child: ListView.builder(
-                    itemCount: infoList.length,
-                    itemBuilder: (context, index){
-                        return Card( child: ListTile(
-                            title: Text(fields[index] + ": " + infoList[index]),
-                        )); 
-                    }
-                )
-            ),
+            body: bodyContactDetail(infoList, phone, context)
         );
     }
 }
