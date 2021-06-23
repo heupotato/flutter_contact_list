357284b69609b70b7c3e3d0badd51de3a29ce1d3
LoanThai <thloanth.dut@gmail.com>
2021-06-23 09:12:24 +0700

    Update Contact Screen
    Update function in mock_data to get contact by ID
    Create update contact screen

--- lib/mockdata/mock_contact.dart
+++ lib/mockdata/mock_contact.dart
@@ -17,6 +17,6 @@ class Contact{
 class MockContact{
     static List<Contact> mockList() => 
         List.generate(10, (int index) => Contact(id: index, firstName: "First ", lastName: "Last", phoneNumber: "0123456789")); 
-    static Contact mockContact(int index) => Contact(id: index, firstName: "First ", lastName: "Last", phoneNumber: "0123456789",
+    static Contact mockContact(int index) => Contact(id: index, firstName: "First ${index.toString()}", lastName: "Last", phoneNumber: "0123456789",
                                                     address: "Nowhere", email: "mail@gmail.com", gender: "Male"); 
 }
