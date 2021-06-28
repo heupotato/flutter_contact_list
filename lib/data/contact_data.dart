aea2e1acf8561d67ccc63f5646b9ef3ea0cd878e
LoanThai <thloanth.dut@gmail.com>
2021-06-28 10:02:01 +0700

    Add Contact database and its adapter

---
+++ lib/data/contact_data.dart
@@ -0,0 +1,31 @@
+import 'package:hive/hive.dart';
+
+part 'contact_data.g.dart';
+@HiveType(typeId: 0)
+class Contact{
+  @HiveField(0)
+  String firstName;
+
+  @HiveField(1)
+  String lastName;
+
+  @HiveField(2)
+  String phoneNumber;
+
+  @HiveField(3)
+  int gender;
+
+  @HiveField(4)
+  String email;
+
+  @HiveField(5)
+  String address;
+
+  Contact({required this.firstName, required this.lastName, required this.phoneNumber,
+    this.address = "", this.email ="", this.gender = 0});
+
+  @override
+  String toString() {
+    return("First name: $firstName\nLast name: $lastName\nTel: $phoneNumber");
+  }
+}
