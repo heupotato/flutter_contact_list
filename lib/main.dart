4887de5a9cfb983ee778b11adf15dfdd5fecf5ae
LoanThai <thloanth.dut@gmail.com>
2021-06-28 10:04:32 +0700

    Add Hive init() and registerAdapter to main
    These functions are to init database

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_contact_list/data/contact_data.dart';
 import 'package:flutter_contact_list/ui/screens/contact_list_screen.dart';
-void main() => runApp(MyApp()); 
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDirectory = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDirectory.path);
+
+  Hive.registerAdapter(ContactAdapter());
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget{
     @override
