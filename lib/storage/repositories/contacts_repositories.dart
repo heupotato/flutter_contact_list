7767e2b51f03cf0fa8fa6047af205e567687e286
LoanThai <thloanth.dut@gmail.com>
2021-06-29 12:09:48 +0700

    Update ContactRepositories
    Shorten the functions

--- lib/storage/repositories/contacts_repositories.dart
+++ lib/storage/repositories/contacts_repositories.dart
@@ -2,20 +2,11 @@ import 'package:flutter_contact_list/data/contact_data.dart';
 import 'package:hive/hive.dart';
 
 class ContactsRepository{
-  static Future<Box<Contact>> loadBox(){
-    return Hive.openBox<Contact>("contacts");
-  }
+  static Future<Box<Contact>> loadBox() => Hive.openBox<Contact>("contacts");
 
-  static Box<Contact> getBox() {
-    return Hive.box<Contact>("contacts");
-  }
+  static Box<Contact> getBox() => Hive.box<Contact>("contacts");
 
-  static List<Contact>? getAllContacts() {
-    final contactsData = getBox();
-    return contactsData.values.toList();
-  }
-  static Contact? getContactInfo(int index){
-    Box contacts = getBox();
-    return contacts.getAt(index);
-  }
+  static List<Contact>? getAllContacts() => getBox().values.toList();
+
+  static Contact? getContactInfo(int index) => getBox().getAt(index);
 }
