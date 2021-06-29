28ce243f1f4325e5cba26b9923170df9e241ef7f
LoanThai <thloanth.dut@gmail.com>
2021-06-29 11:44:32 +0700

    Update contact_detail_screen.dart
    - Change the class' name
    - Change the variable's type from int to Contact in order to pass the whole  Contact object from ContactListScreen

--- lib/ui/screens/contact_detail_screen.dart
+++ lib/ui/screens/contact_detail_screen.dart
@@ -6,18 +6,15 @@ import 'package:flutter_contact_list/ui/widgets/contact_detail.dart';
 import 'package:flutter_contact_list/ui/widgets/contact_header.dart';
 import 'package:hive/hive.dart';
 
-class ContactDetail extends StatelessWidget {
-    final int index;
+class ContactDetailScreen extends StatelessWidget {
+    final Contact contact;
 
-    Contact _contactInfo(){
-        return ContactsRepository.getContactInfo(index);
-    }
-    ContactDetail({Key? key, required this.index}) : super(key: key);
+    ContactDetailScreen({Key? key, required this.contact}) : super(key: key);
 
     @override
     Widget build(BuildContext context) {
         ///get full information of contact by id
-        Contact contactInfo = _contactInfo();
+        Contact contactInfo = contact;
         String contactName = contactInfo.firstName + " " + contactInfo.lastName;
         return Scaffold(
             appBar: AppBar(
@@ -27,7 +24,7 @@ class ContactDetail extends StatelessWidget {
             body: SingleChildScrollView(
                 child: Column(
                     children: [ContactHeader(contactName: contactName),
-                            Detail(contactInfo: contactInfo,)]
+                            ContactDetail(contactInfo: contactInfo,)]
             )
             )
         );
