8cd9958efa0579ae8d68f723cab8d4aae1dc977a
LoanThai <thloanth.dut@gmail.com>
2021-06-29 11:40:50 +0700

    Update contacts_repositories.dart
    - Change Future<void> openbox() to Future<Box<Contact>> loadBox()
    - Change all of the Box<dynamic> to Box<Contact>
    - Shorten gellAllContacts function

--- lib/storage/repositories/contacts_repositories.dart
+++ lib/storage/repositories/contacts_repositories.dart
@@ -2,22 +2,20 @@ import 'package:flutter_contact_list/data/contact_data.dart';
 import 'package:hive/hive.dart';
 
 class ContactsRepository{
-  static List<Contact> getAllContacts() {
-    final contactsData = Hive.box("contacts");
-    List<Contact> contactList = [];
-    for (int i = 0; i < contactsData.length; i++)
-      contactList.add(contactsData.getAt(i) as Contact);
-    return contactList;
+  static Future<Box<Contact>> loadBox(){
+    return Hive.openBox<Contact>("contacts");
   }
 
-  static Contact getContactInfo(int index){
+  static Box<Contact> getBox() {
+    return Hive.box<Contact>("contacts");
+  }
+
+  static List<Contact>? getAllContacts() {
+    final contactsData = getBox();
+    return contactsData.values.toList();
+  }
+  static Contact? getContactInfo(int index){
     Box contacts = getBox();
     return contacts.getAt(index);
-  }
-  static Future<void> openBox(){
-    return Hive.openBox("contacts");
-  }
-  static Box getBox() {
-    return Hive.box("contacts");
   }
 }
