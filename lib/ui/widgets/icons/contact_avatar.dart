582a17825ea57d77c6934c999fb112b0d83fb516
LoanThai <thloanth.dut@gmail.com>
2021-07-01 08:41:31 +0700

    Create contact_avatar.dart

---
+++ lib/ui/widgets/icons/contact_avatar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:mdi/mdi.dart';
+
+class ContactAvatar extends StatelessWidget {
+  final String name; 
+  ContactAvatar({Key? key, required this.name}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: 20,
+      backgroundColor: Colors.teal[400],
+      child: Text(
+        name[0].toUpperCase(),
+        style: TextStyle(
+          fontSize: 25,
+          color: Colors.white
+        )
+      ),
+    );
+  }
+}
