db19bde291732a762e9af6bb898836670fa352d4
LoanThai <thloanth.dut@gmail.com>
2021-07-01 10:19:25 +0700

    Update contact_detail_screen.dart
    Insert button update to Contact_detail_screen's appbar

--- lib/ui/screens/contact_detail_screen.dart
+++ lib/ui/screens/contact_detail_screen.dart
@@ -1,22 +1,41 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_contact_list/data/contact_data.dart';
+import 'package:flutter_contact_list/storage/repositories/contacts_repositories.dart';
+import 'package:flutter_contact_list/ui/screens/update_contact_screen.dart';
 import 'package:flutter_contact_list/ui/widgets/contact_detail.dart';
 import 'package:flutter_contact_list/ui/widgets/contact_header.dart';
 
-class ContactDetailScreen extends StatelessWidget {
-    final Contact contact;
+class ContactDetailScreen extends StatefulWidget {
+    final int id;
 
-    ContactDetailScreen({Key? key, required this.contact}) : super(key: key);
+    ContactDetailScreen({Key? key, required this.id}) : super(key: key);
+
+  @override
+  _ContactDetailScreenState createState() => _ContactDetailScreenState();
+}
+
+class _ContactDetailScreenState extends State<ContactDetailScreen> {
+
+    _getContact(id) => ContactsRepository.getContactInfo(id);
+
+    _gotoUpdateContact() {
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => UpdateContactScreen(id: widget.id))
+        );
+    }
 
     @override
     Widget build(BuildContext context) {
         ///get full information of contact by id
-        Contact contactInfo = contact;
+        Contact contactInfo = _getContact(widget.id);
         String contactName = contactInfo.firstName + " " + contactInfo.lastName;
         return Scaffold(
             appBar: AppBar(
-                title: Text("Contact Detail")
+                title: Text("Contact Detail"),
+                actions: [IconButton(icon: Icon(Icons.edit),
+                    onPressed: _gotoUpdateContact)],
             ),
             resizeToAvoidBottomInset: true,
             body: SingleChildScrollView(
