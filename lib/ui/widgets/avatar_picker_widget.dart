2ea8a5b2124b3a7f1d640d9f4e62cc51a7bd622f
LoanThai <thloanth.dut@gmail.com>
2021-07-02 16:22:52 +0700

    Add avatar feature

---
+++ lib/ui/widgets/avatar_picker_widget.dart
@@ -0,0 +1,130 @@
+import 'dart:io';
+import 'package:flutter_contact_list/pic/assets.dart';
+import 'package:flutter_contact_list/storage/repositories/contacts_repositories.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:image_picker/image_picker.dart';
+
+
+class AvatarPicker extends StatefulWidget {
+  final int index;
+  const AvatarPicker({Key? key, required this.index}) : super(key: key);
+
+  @override
+  _AvatarPickerState createState() => _AvatarPickerState();
+}
+
+class _AvatarPickerState extends State<AvatarPicker> {
+  File ? _image;
+  final _picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    String? path = ContactsRepository.getContactAvatar(widget.index);
+    if (path!= null && path!="")
+      _image = File(path);
+  }
+  _imgFromCamera() async {
+    final image = await _picker.getImage(
+        source: ImageSource.camera, imageQuality: 50);
+    if (image == null) return;
+
+    imageCache!.clear();
+    Directory tempDir = await getApplicationDocumentsDirectory();
+    final String path = tempDir.path;
+    final File imageFile = File(image.path);
+
+    final File newImage = await imageFile.copy('$path/contactAvatar${widget.index}.png');
+    setState(() {
+      _image = newImage;
+      print(newImage.path);
+      ContactsRepository.setContactAvatar(widget.index, newImage.path);
+    });
+  }
+
+  _imgFromGallery() async {
+    final image = await _picker.getImage(
+        source: ImageSource.gallery, imageQuality: 50);
+    if (image == null) return;
+
+    imageCache!.clear();
+    Directory tempDir = await getApplicationDocumentsDirectory();
+    final String path = tempDir.path;
+    final File imageFile = File(image.path);
+    final File newImage = await imageFile.copy('$path/contactAvatar${widget.index}.png');
+    setState(() {
+      _image = newImage;
+      ContactsRepository.setContactAvatar(widget.index, newImage.path);
+    });
+  }
+
+  void _showPicker (context){
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext buildContext){
+          return SafeArea(
+              child: Container(
+                child: Wrap(
+                  children: [
+                    ListTile(
+                      leading: Icon(Icons.photo_library),
+                      title: Text("Gallery"),
+                      onTap: () {
+                        _imgFromGallery();
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.photo_camera),
+                      title: Text("Camera"),
+                      onTap: () {
+                        _imgFromCamera();
+                        Navigator.of(context).pop();
+                      },
+                    )
+                  ],
+                ),
+              )
+          );
+        }
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+            child: GestureDetector(
+              onTap: () {
+                _showPicker(context);
+              },
+              child: CircleAvatar(
+                radius: 100,
+                backgroundColor: Colors.white,
+                child: _image != null
+                    ? ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.file(
+                        _image!,
+                        width: 190,
+                        height: 190,
+                        fit: BoxFit.fill,
+                      ),
+                    )
+                    : Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(100)),
+                      width: 190,
+                      height: 190,
+                      child: Image.asset(
+                        Assets.placeholder,
+                        fit: BoxFit.contain,
+                        height: 150,
+                      )
+                  ),
+                ),
+              ),
+            );
+  }
+}
--- lib/ui/widgets/contact_header.dart
+++ lib/ui/widgets/contact_header.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_contact_list/pic/assets.dart';
+import 'package:flutter_contact_list/ui/widgets/avatar_picker_widget.dart';
 
 class ContactHeader extends StatelessWidget {
-  final String contactName; 
-  const ContactHeader({Key? key, required this.contactName}) : super(key: key);
+  final String contactName;
+  final int index;
+  const ContactHeader({Key? key, required this.contactName, required this.index})
+      : super(key: key);
 
   List<Widget> headerPic(String contactName){
-    return [Image.asset(
-      Assets.placeholder,
-      fit: BoxFit.contain,
-      height: 200,
-    ),
-      SizedBox(height: 20),
+    return [Expanded(child: AvatarPicker(index: index,)),
+      SizedBox(height: 5),
       Text(contactName,
           style: TextStyle(fontFamily: 'LobsterTwo', fontStyle: FontStyle.italic,
               fontWeight: FontWeight.w500, fontSize: 35))
