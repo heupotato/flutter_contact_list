ca7874ef4a5688d35a008f5bfe8257d3d55668e6
LoanThai <thloanth.dut@gmail.com>
2021-07-02 10:55:02 +0700

    Call or email when tap on info tile

--- lib/ui/widgets/contact_detail.dart
+++ lib/ui/widgets/contact_detail.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_contact_list/data/contact_data.dart';
-
+import 'package:url_launcher/url_launcher.dart' as urlLauncher;
 class ContactDetail extends StatelessWidget {
   final Contact contactInfo;
   ContactDetail({Key? key, required this.contactInfo}) : super(key: key);
@@ -66,17 +66,32 @@ class ContactDetail extends StatelessWidget {
     );
   }
 
+  Card actionCard (String title, String info, IconData icon){
+    if (info == "" || (title != "Email" && title != "Phone Number"))
+      return detailInfo(title, info, icon);
+    String action = title == "Phone Number" ? "tel" : "mailto";
+    return Card(
+      shadowColor: Colors.black,
+      child: ListTile(
+        title: shownInfo(info, title),
+        subtitle: Text(title),
+        leading: Icon(icon, color: Colors.teal[400], size: 30),
+        onTap: () => urlLauncher.launch('$action://$info')
+      ),
+    );
+  }
+
   List<Card> detailList(List<String> infoList){
     List<Card> detailList = [];
     for (int i = 0; i<infoList.length; i++)
-      detailList.add(detailInfo(fields[i], infoList[i], iconFields[i]));
+      detailList.add(actionCard(fields[i], infoList[i], iconFields[i]));
     return detailList;
   }
 
   Widget bodyContactDetail(List<String> infoList, String phone, BuildContext context){
     List<Widget> colChildren = [];
     colChildren.addAll(titleObject("Phone"));
-    colChildren.add(detailInfo("Phone Number", phone, Icons.phone));
+    colChildren.add(actionCard("Phone Number", phone, Icons.phone));
     colChildren.addAll(titleObject("Detail"));
     colChildren.addAll(detailList(infoList));
     colChildren.add(SafeArea(child: SizedBox(height: 20)));
